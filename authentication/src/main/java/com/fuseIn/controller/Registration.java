188a4ea04751b0b998b4e64d6fa482d1ea45c254
Ashish-Chaturvedi <ashish.chaturvedi092@gmail.com>
2018-04-26 09:57:30 +0000

    To onject with registration RESt API updated

--- authentication/src/main/java/com/fuseIn/controller/Registration.java
+++ authentication/src/main/java/com/fuseIn/controller/Registration.java
@@ -1,14 +1,15 @@
 package com.fuseIn.controller;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.fuseIn.entity.RegisterUserTO;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
 
 
 @Api(value="/Registration")
@@ -17,8 +18,10 @@ public class Registration {
 
 	@POST
 	@ApiOperation(value = "Submit registration details", httpMethod = "POST", notes = "registration details")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String sayPlainTextHello() {
-		return "Hello World RESTful Jersey!";
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void getRegistrationDetails(RegisterUserTO userRegistration) {
+
+//System.out.println("Hi!! "+userRegistration.getFirstName());
 	}
 }
---
+++ authentication/src/main/java/com/fuseIn/entity/RegisterUserTO.java
@@ -0,0 +1,66 @@
+package com.fuseIn.entity;
+
+import java.util.List;
+
+public class RegisterUserTO {
+	
+	private String firstName;
+	private String lastName;
+	private String contact;
+	private List<Address> address;
+	private String age;
+	private String gender;
+	private String interest;
+	private String email;
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public List<Address> getAddress() {
+		return address;
+	}
+	public void setAddress(List<Address> address) {
+		this.address = address;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getInterest() {
+		return interest;
+	}
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
+	
+}
