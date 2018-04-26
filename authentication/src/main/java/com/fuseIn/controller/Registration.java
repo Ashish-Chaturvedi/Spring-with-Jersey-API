195e43e7d59919b48441f8423ca676fe1d9e0728
Ashish-Chaturvedi <ashish.chaturvedi092@gmail.com>
2018-04-26 10:13:22 +0000

    response updated

--- authentication/src/main/java/com/fuseIn/controller/Registration.java
+++ authentication/src/main/java/com/fuseIn/controller/Registration.java
@@ -5,11 +5,13 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.fuseIn.entity.RegisterUserTO;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 
 @Api(value="/Registration")
@@ -20,8 +22,7 @@ public class Registration {
 	@ApiOperation(value = "Submit registration details", httpMethod = "POST", notes = "registration details")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void getRegistrationDetails(RegisterUserTO userRegistration) {
-
-//System.out.println("Hi!! "+userRegistration.getFirstName());
+	public Response getRegistrationDetails(@ApiParam(value = "User addition to portal", required = true) RegisterUserTO userRegistration) {
+		return Response.ok().entity(userRegistration).build();
 	}
 }
