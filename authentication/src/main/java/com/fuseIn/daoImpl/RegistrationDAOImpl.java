bae8947b004ad01082d2713ba50afb48b203be14
Ashish-Chaturvedi <ashish.chaturvedi092@gmail.com>
2018-05-04 07:14:22 +0000

    connection added to authentication

--- authentication/src/main/java/com/fuseIn/daoImpl/RegistrationDAOImpl.java
+++ authentication/src/main/java/com/fuseIn/daoImpl/RegistrationDAOImpl.java
@@ -2,7 +2,9 @@ package com.fuseIn.daoImpl;
 
 import org.springframework.stereotype.Repository;
 
+import com.datastax.driver.core.Session;
 import com.fuseIn.Iservice.IRegisterDao;
+import com.fuseIn.connector.Cassandra;
 import com.fuseIn.dao.RegisterUserDAO;
 /*
  * 
@@ -14,6 +16,9 @@ import com.fuseIn.dao.RegisterUserDAO;
 public class RegistrationDAOImpl implements IRegisterDao{
 
 	public void create(RegisterUserDAO userDao) {
+		
+	Cassandra cassandraConn = new Cassandra();
+	Session session = cassandraConn.getSession();
 		
 	}
 }
